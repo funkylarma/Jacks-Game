83fa5b2e3dac98c7c803fbcd8f6219242f912134
Adam Chamberlin <adam@shibbyonline.co.uk>
2013-07-24 08:45:28 +0100

    Initial Commit

---
+++ Jacks Game/SVDViewController.m
@@ -0,0 +1,29 @@
+//
+//  SVDViewController.m
+//  Jacks Game
+//
+//  Created by Adam Chamberlin on 24/07/2013.
+//  Copyright (c) 2013 Stour Valley Designs. All rights reserved.
+//
+
+#import "SVDViewController.h"
+
+@interface SVDViewController ()
+
+@end
+
+@implementation SVDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
