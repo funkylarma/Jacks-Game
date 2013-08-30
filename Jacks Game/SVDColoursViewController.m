43dfb2c78f958118e249a2df2db7b01469582ad4
Adam Chamberlin <adam@shibbyonline.co.uk>
2013-08-30 22:27:09 +0100

    fix UIColour issue

--- Jacks Game/SVDColoursViewController.m
+++ Jacks Game/SVDColoursViewController.m
@@ -23,13 +23,13 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         self.colours = @{
-                         @"Red"    : [UIColor colorWithRed:215 green:0   blue:20  alpha:100],
-                         @"Green"  : [UIColor colorWithRed:121 green:207 blue:25  alpha:100],
-                         @"Yellow" : [UIColor colorWithRed:251 green:229 blue:0   alpha:100],
-                         @"Blue"   : [UIColor colorWithRed:56  green:146 blue:227 alpha:100],
-                         @"Orange" : [UIColor colorWithRed:251 green:166 blue:23  alpha:100],
-                         @"Purple" : [UIColor colorWithRed:143 green:0   blue:255 alpha:100],
-                         @"Brown"  : [UIColor colorWithRed:142 green:88  blue:40  alpha:100],
+                         @"Red"    : [UIColor colorWithRed:215.0/255.0 green:0.0/255.0   blue:20.0/255.0  alpha:100],
+                         @"Green"  : [UIColor colorWithRed:121.0/255.0 green:207.0/255.0 blue:25.0/255.0  alpha:100],
+                         @"Yellow" : [UIColor colorWithRed:251.0/255.0 green:229.0/255.0 blue:0.0/255.0   alpha:100],
+                         @"Blue"   : [UIColor colorWithRed:56.0/255.0  green:146.0/255.0 blue:227.0/255.0 alpha:100],
+                         @"Orange" : [UIColor colorWithRed:251.0/255.0 green:166.0/255.0 blue:23.0/255.0  alpha:100],
+                         @"Purple" : [UIColor colorWithRed:143.0/255.0 green:0.0/255.0   blue:255.0/255.0 alpha:100],
+                         @"Brown"  : [UIColor colorWithRed:142.0/255.0 green:88.0/255.0  blue:40.0/255.0  alpha:100],
                          };
         self.index = @[
                        @"Red",
@@ -55,9 +55,17 @@
 
 - (void)displayColourAtIndex:(int)index
 {
+    // Set string value for the colour
     NSString *colour = self.index[index];
+    
+    // Set the alpha levels of the images
+    [self.imgColour setAlpha:0.0];
+    
+    // Load the images
+    self.imgColour.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@.png", colour]];
+    
+    // Set the background colour
     self.view.backgroundColor = self.colours[colour];
-    self.lblColour.text = colour;
 }
 
 - (void)didReceiveMemoryWarning
@@ -66,7 +74,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)btnNext:(id)sender {
+- (IBAction)btnNext:(id)sender
+{
     self.counter++;
     if (self.counter < [self.index count]) {
         [self displayColourAtIndex:self.counter];
@@ -76,13 +85,28 @@
     }
 }
 
-- (IBAction)btnPrev:(id)sender {
+- (IBAction)btnPrev:(id)sender
+{
     self.counter--;
     if (self.counter >= 0) {
         [self displayColourAtIndex:self.counter];
     } else {
         self.counter = 6;
         [self displayColourAtIndex:self.counter];
+    }
+}
+
+- (IBAction)toggleDisplay:(id)sender
+{
+    // Check to see what image is visible
+    if (self.imgColour.alpha == 0.0) {
+        [UIView animateWithDuration:0.4 animations:^{
+            [self.imgColour setAlpha:1.0];
+        }];
+    } else {
+        [UIView animateWithDuration:0.4 animations:^{
+            [self.imgColour setAlpha:0.0];
+        }];
     }
 }
 
