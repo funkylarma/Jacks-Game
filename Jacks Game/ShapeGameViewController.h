c3f9b139ac63e469f38281d090a6779e60e55496
Adam Chamberlin <adam@shibbyonline.co.uk>
2013-11-27 14:00:18 +0000

    Add Shape game view controller

---
+++ Jacks Game/ShapeGameViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShapeGameViewController.h
+//  Jacks Game
+//
+//  Created by Adam Chamberlin on 27/11/2013.
+//  Copyright (c) 2013 Stour Valley Designs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SVDGameViewController.h"
+
+@interface ShapeGameViewController : SVDGameViewController
+
+@end
---
+++ Jacks Game/ShapeGameViewController.m
@@ -0,0 +1,13 @@
+//
+//  ShapeGameViewController.m
+//  Jacks Game
+//
+//  Created by Adam Chamberlin on 27/11/2013.
+//  Copyright (c) 2013 Stour Valley Designs. All rights reserved.
+//
+
+#import "ShapeGameViewController.h"
+
+@implementation ShapeGameViewController
+
+@end
