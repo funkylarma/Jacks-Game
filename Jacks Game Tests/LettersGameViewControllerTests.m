d8c89aa59a71a84f4c95bc944f7d4e2bf9322004
Adam Chamberlin <adam@shibbyonline.co.uk>
2014-03-01 09:09:36 +0000

    added tests and clean up code

---
+++ Jacks Game Tests/LettersGameViewControllerTests.m
@@ -0,0 +1,31 @@
+//
+//  LettersGameViewControllerTests.m
+//  Jacks Game
+//
+//  Created by Adam Chamberlin on 01/03/2014.
+//  Copyright (c) 2014 Stour Valley Designs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LettersGameViewControllerTests : XCTestCase
+
+@end
+
+@implementation LettersGameViewControllerTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown {
+  // Put teardown code here; it will be run once, after the last test case.
+  [super tearDown];
+}
+
+- (void)testExample {
+  XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ Jacks Game Tests/ShapeGameViewControllerTests.m
@@ -0,0 +1,31 @@
+//
+//  ShapeGameViewControllerTests.m
+//  Jacks Game
+//
+//  Created by Adam Chamberlin on 01/03/2014.
+//  Copyright (c) 2014 Stour Valley Designs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShapeGameViewControllerTests : XCTestCase
+
+@end
+
+@implementation ShapeGameViewControllerTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown {
+  // Put teardown code here; it will be run once, after the last test case.
+  [super tearDown];
+}
+
+- (void)testExample {
+  XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
